140414d890177fa233b63882980f93a8ea537b9d
mariogk <mariogk01@gmail.com>
2024-01-23 09:47:40 +0000

    Initial commit

---
+++ src/AspNet8/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+using AspNet8;
+
+namespace AspNet8.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
